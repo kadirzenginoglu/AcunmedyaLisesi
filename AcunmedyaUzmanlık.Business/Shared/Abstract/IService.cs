fe68683d340125152d30f19ddf6e444fff5d0f55
KAdir Zenginoglu <kadir@zenginoglu.com>
2024-07-03 23:52:45 +0300

    Buseiness Katmanı Oluşturuldu

---
+++ AcunmedyaUzmanlık.Business/Shared/Abstract/IService.cs
@@ -0,0 +1,23 @@
+﻿using AcunmedyaLisesi.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcunmedyaUzmanlık.Business.Shared.Abstract
+{
+    public interface IService<T> where T : BaseModel
+    {
+        ICollection<T> GetAll();
+        T Add(T entity);
+        List<T> AddRange(List<T> entities);
+        T Update(T entity);
+        void Delete(int id);
+        T GetById(int id);
+
+        T GetFirstOrDefault(Expression<Func<T, bool>> predicate);
+        
+    }
+}
---
+++ AcunmedyaUzmanlık.Business/Shared/Concrete/Service.cs
@@ -0,0 +1,59 @@
+﻿using AcunmedyaLisesi.Repository.Shared.Abstract;
+using AcunmedyaLisesi.Web.Models;
+using AcunmedyaUzmanlık.Business.Shared.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcunmedyaUzmanlık.Business.Shared.Concrete
+{
+    public class Service<T> : IService<T> where T : BaseModel
+    {
+        private readonly IRepository<T> _repo;
+
+        public Service(IRepository<T> repo)
+        {
+            _repo = repo;
+        }
+
+        public T Add(T entity)
+        {
+            return _repo.Add(entity);
+        }
+
+        public List<T> AddRange(List<T> entities)
+        {
+            _repo.AddRange(entities);
+            Save();
+            return entities;
+        }
+
+        public void Delete(int id)
+        {
+            _repo.Delete(id);
+        }
+
+        public ICollection<T> GetAll()
+        {
+            return _repo.GetAll().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return _repo.GetFirstOrDefault(predicate);
+        }
+        public T Update(T entity)
+        {
+            return _repo.Update(entity);
+        }
+    }
+}
