69459d08dfba1d158640d1dc3e4cd32cf219a8bd
KAdir Zenginoglu <kadir@zenginoglu.com>
2024-07-03 22:47:06 +0300

    Controller Katmanı Detaylandırıldı

---
+++ AcunmedyaLisesi.Web/Controllers/SinavlarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcunmedyaLisesi.Web.Controllers
+{
+    public class SinavlarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
